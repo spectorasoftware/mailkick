38ae343cf50698aebe197b4aaf9d045b9744da6d
Daniel Aguilar <daniel@spectora.com>
2026-01-16 12:09:21 -0600

    chore(COR-3407): add support for Rails 6.1

--- lib/mailkick/engine.rb
+++ lib/mailkick/engine.rb
@@ -6,7 +6,10 @@ module Mailkick
       Mailkick.discover_services unless Mailkick.services.any?
 
       # avoid app.key_generator due to https://github.com/ankane/ahoy_email/pull/168
-      Mailkick.secret_token ||= ActiveSupport::KeyGenerator.new(app.secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1).generate_key("mailkick")
+      # hash_digest_class option added in Rails 7.0; Rails 6.1 uses SHA1 by default
+      key_generator_options = { iterations: 1000 }
+      key_generator_options[:hash_digest_class] = OpenSSL::Digest::SHA1 if Rails::VERSION::MAJOR >= 7
+      Mailkick.secret_token ||= ActiveSupport::KeyGenerator.new(app.secret_key_base, **key_generator_options).generate_key("mailkick")
     end
   end
 end
